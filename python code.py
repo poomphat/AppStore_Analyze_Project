3eaae4562b73c5f9dd23aca7a410b34c811b08d3
poomphat wawongmoon <pumphat@hotmail.co.th>
2018-12-02 12:23:59 +0700

    sort price
    add sort_value("สิ่งที่ต้องการ sort")

--- python code.py
+++ python code.py
@@ -1,4 +1,6 @@
 import numpy as np
 import pandas as pd
+import json
 data_file = pd.read_csv('AppleStore.csv')
-print(data_file[0:-1])
+data_file.sort_values(by=['price'])
+print(data_file.sort_values(by=['price']))
