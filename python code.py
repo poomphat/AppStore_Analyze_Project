c0b93046004d88420235b4d6007cda36a556e23f
poomphat wawongmoon <pumphat@hotmail.co.th>
2018-12-10 17:16:08 +0700

    add description

--- python code.py
+++ python code.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pandas as pd
 import json
+#open file AppleSotore.CSV 
 data_file = pd.read_csv('AppleStore.csv')
+#Sort price
 data_file.sort_values(by=['price'])
+#output datafile
 print(data_file.sort_values(by=['price']))
